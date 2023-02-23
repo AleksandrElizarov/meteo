7044845996677a9be8a979d25123553f39fc5d99
Aleksandr Elizarov <aleksandr.elizar@mail.ru>
2023-02-23 14:08:08 +0600

    Feat: add html code

--- meteo/meteo_app/views.py
+++ meteo/meteo_app/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 import requests
 from loguru import logger
+import datetime
 from meteo.settings import settings
 
 url_find_current_weather_by_city_name = "http://api.openweathermap.org/data/2.5/find"  # Адрес для запроса погоды по городу
 url_find_5days_weather_by_city_id = "http://api.openweathermap.org/data/2.5/forecast"
+months_dict = {
+    "January": "января",
+    "February": "февраля",
+    "March": "марта",
+    "April": "апреля",
+    "May": "мая",
+    "June": "июня",
+    "July": "июля",
+    "August": "августа",
+    "September": "сентября",
+    "October": "октября",
+    "November": "ноября",
+    "December": "декабря"
+}
 
 
 def view_meteo_page(request):
@@ -30,7 +45,16 @@ def view_meteo_page(request):
         data_list_5days_weather = res_data_5days.json()
         logger.info(f"Response meteo data 5 days: {data_list_5days_weather}")
 
-        return render(request, 'meteo_success.html')
+        first_day_date_str = data_list_5days_weather['list'][0]['dt_txt']
+        obj_first_day_date_str = datetime.datetime.strptime(first_day_date_str, '%Y-%m-%d %H:%M:%S')
+        first_day = obj_first_day_date_str.strftime('%d')
+        formatted_first_month = obj_first_day_date_str.strftime('%B')
+        first_month = months_dict[formatted_first_month]
+        dict_weather_data_5days = {'city_name': city_name,
+                                   'first_day': first_day,
+                                   'first_month': first_month}
+
+        return render(request, 'meteo_success.html', context=dict_weather_data_5days)
 
     except Exception as e:
         logger.error(f"Exception, not data from response: {e}")
