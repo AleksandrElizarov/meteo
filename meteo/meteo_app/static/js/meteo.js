cedbd5063fcd911bcbb5e026b7546137ec221173
Aleksandr Elizarov <aleksandr.elizar@mail.ru>
2023-02-25 10:53:52 +0600

    Feat: Modify element_wind_deg, view word output

--- meteo/meteo_app/static/js/meteo.js
+++ meteo/meteo_app/static/js/meteo.js
@@ -1,7 +1,7 @@
 //Вычисление background-color для content_param_value
-var custom_color_categories = ["#1333ee", "#4febeb", "#dae90e", "#e526eb", "#e45252"];
+const custom_color_categories = ["#1333ee", "#4febeb", "#dae90e", "#e526eb", "#e45252"];
 
-var limits_temperature = [-10, 5, 20, 30];
+const limits_temperature = [-10, 5, 20, 30];
 set_color_element_by_id(ElementById='temperature', 
                         arr_limits=limits_temperature, 
                         arr_color_cat=custom_color_categories)
@@ -21,11 +21,6 @@ set_color_element_by_id(ElementById='wind_speed',
                         arr_limits=limits_wind_speed, 
                         arr_color_cat=custom_color_categories)                         
 
-
-
-
-
-
 //Функция опредление background-color по значениям text'a getElementById
 function set_color_element_by_id(ElementById, arr_limits, arr_color_cat){
     let element_id = document.getElementById(ElementById);
@@ -42,4 +37,12 @@ function set_color_element_by_id(ElementById, arr_limits, arr_color_cat){
         element_id.style.backgroundColor = arr_color_cat[4];    
     }
 }
+
+//Вычисление направления ветра(юг, север, запад, восток и т.д. в зависимости от направления азимута в градусах)
+const directions = ['Север.', 'СВ', 'Восточ.', 'ЮВ', 'Южн.', 'ЮЗ', 'Запад.', 'СЗ'];
+let element_wind_deg = document.getElementById('wind_deg');
+let angle = document.getElementById('wind_deg').textContent;
+let index = Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8;
+element_wind_deg.innerText = directions[index];
+
 
